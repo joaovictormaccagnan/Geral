585d798201cc4765247e4dafcccc95d538009bf4
joaovictormaccagnan <148975950+joaovictormaccagnan@users.noreply.github.com>
2026-02-25 12:16:09 -0300

    Add files via upload

---
+++ gera_cpf.py
@@ -0,0 +1,29 @@
+cpf = '39026812094'
+nove_digitos = cpf [:9] 
+indice = 10
+adicao = 0
+digito_1 = 0
+for d in nove_digitos:
+    adicao += int(d) * indice
+    indice -= 1
+digito_1 += (adicao * 10) % 11
+digito_1 = digito_1 if digito_1 <= 9 else 0
+
+
+indice = 11
+digito_2 = 0
+adicao_2 = 0
+dez_digito = 0
+dez_digito = nove_digitos + str(digito_1)
+
+for n in dez_digito:
+    adicao_2 += int(n) * indice
+    indice -= 1
+digito_2 += (adicao_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+cpf_gerado = nove_digitos + str(digito_1) + str(digito_2)
+if cpf == cpf_gerado:
+    print("Seu CPF esta correto")
+else:
+    print("CPF invalido")
