69cd7d2f9047046eba77f004ad1bcaa9a5701e4b
Pamzinho <maccagnan.matos@escola.pr.gov.br>
2026-03-02 08:28:46 -0300

    Adicionando novo exercício

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
-print("Bem-vindo ao FizzBuzz!")
-numero = input("Digite um número: ")
-fizz = 0
-buzz = 0
-fizzbuzz = 0
-try:
-    num_int = int(numero) 
-    if num_int <= 0:
-        raise ValueError
-except:
-    print("Não é um número inteiro positivo")
-    exit()
-    
-for i in range(1,num_int + 1):
-    
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-        fizzbuzz += 1
-    elif i % 3 == 0:
-        print("Fizz")
-        fizz += 1
-    elif i % 5 == 0:
-        print("Buzz")
-        buzz += 1
-    else:
-        print(i)
-    
-print(f"fizz = {fizz}, buzz = {buzz}, fizzbuzz = {fizzbuzz}")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Lista que funciona como "banco de dados"
+produtos = []
+
+@app.get("/")
+def inicio():
+    return {"mensagem": "API esta rodando 🚀🚀"}
+
+@app.post("/produtos")
+def criar_produto(nome: str, preco: float):
+    produto = {
+        "id": len(produtos) + 1,
+        "nome": nome,
+        "preco": preco
+    }
+    produtos.append(produto)
+    return produto
+
+@app.get("/produtos")
+def listar_produtos():
+    return produtos
+
+@app.delete("/produtos/{produto_id}")
+def deletar_produto(produto_id: int):
+    for produto in produtos:
+        if produto["id"] == produto_id:
+            produtos.remove(produto)
+            return {"mensagem": "Produto removido"}
+    return {"erro": "Produto não encontrado"}
