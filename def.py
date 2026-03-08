99c267caaf9c7158857990ca6a9ba6e0e0d6430a
Pam <joaovictormatos1414@gmail.com>
2026-03-08 14:46:20 -0300

    Novo

--- def.py
+++ def.py
@@ -3,10 +3,15 @@ def funcao(multiplicador):
         return numero * multiplicador
     return multiplicar
         
-
 duplicar = funcao(2)
 triplicar = funcao(3)
 quadruplicar = funcao(4)
-print(duplicar(2))
-print(triplicar(3))
-print(quadruplicar(4))
+print(duplicar(4))
+print(triplicar(6))
+print(quadruplicar(8))
+"""
+def funcao(2):"
+    def multiplicar(4):
+        return numero * multiplicar
+    return multiplicar
+"""
